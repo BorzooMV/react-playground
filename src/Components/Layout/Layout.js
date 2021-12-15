45d23890aa292583d2a42107da11fccffe224f00
BorzooMV <borzoomoazami94@gmail.com>
2021-12-15 15:14:15 +0330

    Feat: Add dark / light mode button

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -6,24 +6,26 @@ import Sidebar from "../Sidebar/Sidebar";
 import Main from "../Main/Main";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PageLoader from "../PageLoader/PageLoader";
+import Theme from "../../Context/themeContext";
 
 const Layout = () => {
-
     const Home = lazy(()=> import('../Pages/Home/Home'));
     const Documentation = lazy(()=> import('../Pages/Documentation/Documentation'));
     return(
         <>
             <Suspense fallback={<PageLoader />}>
                 <BrowserRouter>
-                    <Header />
-                    <Sidebar />
-                    <Main>
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/docs" element={<Documentation />} />
-                        </Routes>
-                    </Main>
-                    <Footer />
+                    <Theme.Provider value={"light"}>
+                        <Header />
+                        <Sidebar />
+                        <Main>
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/docs" element={<Documentation />} />
+                            </Routes>
+                        </Main>
+                        <Footer />
+                    </Theme.Provider>
                 </BrowserRouter>
             </Suspense>
         </>
